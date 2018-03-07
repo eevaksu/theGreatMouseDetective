3bfa8fada524f848b2c92c7fabe21d0f415688c7
eevaksu <eevaksu@gmail.com>
2018-03-07 14:00:37 +0000

    added quatation mark to vertigo

--- textUtilities.js
+++ textUtilities.js
@@ -8,7 +8,7 @@ function titleCase (title) {
   return titleCasedWords.join('');
 
 }
-expect(titleCase('the great mouse detective').to.be.a('string');
+expect(titleCase('the great mouse detective')).to.be.a('string');
 expect(titleCase('a')).to.equal('A');
-expect(titleCase('vertigo)).to.equal('Vertigo');
+expect(titleCase('vertigo')).to.equal('Vertigo');
 expect (titleCase('the great mouse detective')).to.equal('The Great Mouse Detective');
